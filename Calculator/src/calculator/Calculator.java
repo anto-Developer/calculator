08790696b49165e6f8dc87fd5f9107ba60a0fe6c
anton <anton@172.19.171.108>
2022-10-20 06:10:38 +0200

    my first commit

--- Calculator/src/calculator/Calculator.java
+++ Calculator/src/calculator/Calculator.java
@@ -17,6 +17,8 @@ public class Calculator {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Hello world");
+        System.out.println("Hello world");
+        
     }
     
 }
