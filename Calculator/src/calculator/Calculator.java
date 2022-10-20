400b150b9053c1b870a188df44ea09dd38ddc514
Emilio-Amato <99982597+Emilio-Amato@users.noreply.github.com>
2022-10-20 16:31:51 +0200

    Emilio's committ

--- Calculator/src/calculator/Calculator.java
+++ Calculator/src/calculator/Calculator.java
@@ -17,6 +17,7 @@ public class Calculator {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Hello world");
+        //test
     }
     
 }
